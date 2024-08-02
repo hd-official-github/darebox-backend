67194aff9f7fb51cc2107bca1255b0befa72c22e
t5-himanshu <himanshu.dubey@tier5.in>
2024-08-02 15:20:24 +0530

    env-remove-api-midware

--- src/app/middlewares/apiMiddleware.ts
+++ src/app/middlewares/apiMiddleware.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { decodeProtectedHeader, jwtVerify } from 'jose';
-
+interface JwtPayload {
+  role: string;
+  
+}
+interface CustomJwtPayload extends JwtPayload {
+  role: string;
+  // Add other properties as needed
+}
 const JWT_SECRET = process.env.NEXTAUTH_SECRET || 'your-secret-key';
 export async function middleware(req: NextRequest) {
 
@@ -18,8 +25,9 @@ export async function middleware(req: NextRequest) {
     const { payload, protectedHeader } = await jwtVerify(token, new TextEncoder().encode(JWT_SECRET), {
       algorithms: ['HS256'],
     });
+    const role = payload.role as string
     const response = NextResponse.next();
-    response.headers.set('user-custom-role', payload.role);
+    response.headers.set('user-custom-role', role);
     return response;
   } catch (error) {
     console.log('err ', error);
